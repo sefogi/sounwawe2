ce046306f5e108f7e0a0af428396fcf87b85e975
sefogi <sebast18@gmail.com>
2024-02-13 15:22:38 +0100

    "up3"

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import "./home.css"
 
 
 export const Home = () => {
   return (
     <div>
-      <div className="jumbotron">
-        <h1 className="display-4">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-4">
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+              <h1>aqui va la imagen</h1>
+          </Col>
+          <Col>
+            <div className="jumbotron">
+              <h1 className="display-4">Hello, world!</h1>
+              <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+              <hr className="my-4" />
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
 
   )
